d121e3040dc0c4d77cea3c0eb4084c78b3abbcfd
unknown <deepglory@gmail.com>
2021-03-18 14:38:46 +0530

    first demo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router,Route,Link ,Switch} from "react-router-dom";
 import Employement from './components/Pages/Employement';
-
+import DashBoard from "./components/Pages/DashBoard";
 
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Employement}/>
-      </Switch>
-    </Router>
-  
+    
+  <DashBoard/>
     
     </div>
   );
